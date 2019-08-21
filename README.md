ba2431016940c8073e3d0594fd36da51351e264b
Samanyu Neelson <samanyuneel@gmail.com>
2019-08-21 20:55:38 +0530

    python and C

--- README.md
+++
@@ -1,3 +0,0 @@
-# Cpractice
-
-exploring!!
--- multable.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//C program to read an integer and print its multiplication table.
-int main(void)
-{
-  int a,mul,pro;
-  printf("enter number:");
-  scanf ("%d", &a);
-  for (mul = 1; mul <= 10; mul++)
-}
