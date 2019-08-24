d252ab8cf0ed0ad5c8918300ba368b1c07dc7d45
Samanyu Neelson <samanyuneel@gmail.com>
2019-08-24 11:33:48 +0530

    new python files

---
+++ python/string.py
@@ -0,0 +1,23 @@
+a = "hello"
+print (a)
+
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+print (b)
+
+# Strings are arrays
+
+c =  "HelloWorld "
+print (c)
+print(c[0:4])  #last position not included 
+
+#negative indexing
+
+print(c[-5:-1])
+
+#lowercase,uppercase,replace,split
+
+print(c.lower())
